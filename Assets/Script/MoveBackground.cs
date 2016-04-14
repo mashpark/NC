82e32a259c1689c85de8fa0ed3ddff1f512f98a1
Miyoung Park <peggy@grampus.co>
2016-04-14 18:30:41 +0900

    Modify script hierachy.

---
+++ Assets/Script/MoveBackground.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBackground : MonoBehaviour {
+	public UISprite	back_0;
+	public UISprite back_1;
+
+	// Update is called once per frame
+	void Update () {
+		
+		back_0.transform.Translate(-0.01f, 0, 0);
+		back_1.transform.Translate(-0.01f, 0, 0);
+
+		if (back_0.transform.localPosition.x < -469.0f) 
+		{
+			back_1.transform.localPosition = new Vector3 (1580.0f, 0, 0);
+		}
+
+		if(back_1.transform.localPosition.x < -469.0f)
+			back_0.transform.localPosition = new Vector3 (1580.0f, 0, 0);
+	}
+}
